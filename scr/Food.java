c7bc738c06c0c45875ab3a8ecf4815f244d7aae1
ArjunramPal <44159014+arjunrampal98@users.noreply.github.com>
2019-09-17 21:28:40 +0530

    Create Food.java

---
+++ scr/Food.java
@@ -0,0 +1,23 @@
+public class Food
+{
+    int itemno;
+    int quantity;   
+    float price;
+    
+    Food(int itemno,int quantity)
+    {
+        this.itemno=itemno;
+        this.quantity=quantity;
+        switch(itemno)
+        {
+            case 1:price=quantity*50;
+                break;
+            case 2:price=quantity*60;
+                break;
+            case 3:price=quantity*70;
+                break;
+            case 4:price=quantity*30;
+                break;
+        }
+    }
+}
